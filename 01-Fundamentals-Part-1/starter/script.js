46ceef34cc46149856bd87381dd8c34be0926f3b
AntoanStefanov <antoine.stefanov@gmail.com>
2022-11-27 13:36:35 +0200

    notes and linking js file to html.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+// console.log('Hello world!');
+let message = 'Hello world!';
+let isJScool = true;
+if (isJScool) {
+  alert(message);
+  console.log(
+    'when the js file is in same folder as the html file, src="script.js", full path is NOT needed.'
+  );
+}
