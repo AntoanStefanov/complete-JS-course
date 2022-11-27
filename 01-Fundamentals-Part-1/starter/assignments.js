a4d3d22642af8fcf5a4480d05a5dd4919b982024
AntoanStefanov <antoine.stefanov@gmail.com>
2022-11-27 21:43:43 +0200

    lecture and assignment for 12. Data Types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,13 @@
+// 10. Values and Variables
 let country = 'Bulgaria';
 let contient = 'Europe';
 let population = 6.4;
 
 console.log(country, contient, population);
+
+// 12. Data Types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,33 @@
+// 10. Values and Variables
+/*
 console.log('Tony');
 console.log(23);
 
 let firstName = 'Antoan';
 console.log(firstName);
+
+let PI = 3.14;
+*/
+
+// 12. Data types
+true; // boolean type
+// console.log(true);
+
+let javascriptIsFun = true; // boolean type
+
+// console.log(javascriptIsFun);
+// console.log(typeof true); // typeof is an operator just like +, - operators.
+console.log(typeof javascriptIsFun);
+// console.log(typeof 21);
+// console.log(typeof 'Hello');
+
+javascriptIsFun = 21;
+console.log(typeof javascriptIsFun);
+
+let firstName; // defining a varaible WITH NO value.
+console.log(firstName); // returns undefined as value of the variable.
+console.log(typeof firstName); // returns undefined as type in string format.
+
+// error in typeof operator!
+console.log(typeof null); // returns object, which is bug. It should return null just lke undefined data type
+// not resolved because of legacy reasons.
