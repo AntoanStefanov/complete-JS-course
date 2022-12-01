459efa0b575d63f42cbf9dd82a53f89df3f7b62e
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-01 15:35:25 +0200

    33. Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 // at the start of each script. ALWAYS. u can add comment before strict, but no code.
 // it shows more errors and forbids us to do certain things. also activates a reserved words list.
 // ex. 'interface'.
@@ -15,3 +16,27 @@ if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('He has drivers license');
 
 // const interface = 'a';
+
+*/
+
+// 33. Functions
+
+function logger() {
+  // function body.
+  console.log('My name is Antoan');
+}
+
+logger(); // invoking the function / running the function / calling the function.
+logger();
+logger();
+
+// fruit here is a parameter.
+function fruitProcessor(fruit) {
+  return `${fruit} juice`;
+}
+
+const juiceMade = fruitProcessor('apple'); // here 'apple' is an argument.
+
+console.log(juiceMade);
+
+console.log(fruitProcessor('banana')); // here 'banana' is an argument.
