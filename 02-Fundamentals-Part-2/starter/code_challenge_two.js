d8f29f5b5c920f499b8d22d184503524366872ec
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-03 14:03:05 +0200

    code challenge two and important notes

--- 02-Fundamentals-Part-2/starter/code_challenge_two.js
+++ 02-Fundamentals-Part-2/starter/code_challenge_two.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// ! Use TERNARY Operator, if chance given......., instead of if/else statement... Cleaner!
 const calcTip = (bill) =>
   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
@@ -14,6 +15,8 @@ console.log(bills);
 // values (so don't store the tip values in separate variables first, but right in the new
 // array).
 
+// A lot cleaner, than making first variables,
+// and then adding them as elements to the array.
 const tips = [
   calcTip(bills[0]),
   calcTip(bills[1]),
@@ -25,3 +28,11 @@ console.log(tips);
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(total);
+
+// In JavaScript, however, functions are not only syntax but also values,
+// which means they can be assigned to variables,
+// stored in the properties of objects or the elements of arrays,
+// passed as arguments to functions, and so on.
+// https://www.oreilly.com/library/view/javascript-the-definitive/9781449393854/ch08s04.html
+
+// ! Use TERNARY Operator, if chance given......., instead of if/else statement... Cleaner! Check google if doubt ?
