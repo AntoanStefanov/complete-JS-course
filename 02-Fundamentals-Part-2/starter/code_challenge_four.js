c0b76b76df9da3e0ad601e25fe6fa11d076d0c8f
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-06 13:37:16 +0200

    challenge finished

--- 02-Fundamentals-Part-2/starter/code_challenge_four.js
+++ 02-Fundamentals-Part-2/starter/code_challenge_four.js
@@ -4,14 +4,15 @@ const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 
-// 50 <= bill && bill <= 300
-const calcTip = (bill) =>
-  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const calcTip = (bill) => {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
 
-for (let index = 0; index < bills.length - 1; index++) {
-  tips.push(calcTip(bills[index]));
-  totals.push(tips[index] + bills[index]);
+for (let index = 0; index < bills.length; index++) {
+  tips.push(calcTip(bills[index])); // tips[index] = calcTip(bills[index]));
+  totals.push(tips[index] + bills[index]); // totals[index] = tips[index] + bills[index];
 }
+
 console.log(bills);
 console.log(tips);
 console.log(totals);
@@ -25,3 +26,5 @@ const calcAverage = function (arr) {
 };
 
 console.log(calcAverage(bills));
+console.log(calcAverage(totals));
+
