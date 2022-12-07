9d9a23062ebe79502868643c2612e2670daf327c
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-07 11:48:15 +0200

    challenge 59 lec

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,44 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
-if (x === 23) console.log(23);
-console.log('test');
+// 59. Using Google, StackOverflow and MDN.
+
+// Given an array of temperatures of one day, calculate the temperature amplitude.
+// Keep in mind that sometimes there might be a sensor error.
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1. Understand the problem.
+// 2. Breaking up into sub-problems.
+
+// 1.1. How to calculate the temperature amplitude ?
+// 1.2. What values are valid for the temperature amplitude to be calculated ?
+
+// 2.1. Iterate over the temperatures / array /.
+// 2.2. Check if the temperature is actually a number, throw error, if is anything else.
+// 2.3. If temperature is valid, calculate the temperature amplitude (somehow).
+
+const calcTempAmplitude = function (temperatures) {
+  let maxTemp = Number.MIN_SAFE_INTEGER;
+  let minTemp = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < temperatures.length; i++) {
+    const currentTemp = temperatures[i];
+
+    if (typeof currentTemp !== 'number') {
+      continue;
+    }
+
+    if (currentTemp < minTemp) {
+      minTemp = currentTemp;
+    }
+
+    if (currentTemp > maxTemp) {
+      maxTemp = currentTemp;
+    }
+  }
+  return maxTemp - minTemp;
+};
+
+const tempAmplitude = calcTempAmplitude(temperatures);
+console.log(tempAmplitude);
