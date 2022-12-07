a85e08e03d1d093d007742fb49863250f259ff24
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-07 14:35:30 +0200

    tests

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -3,7 +3,7 @@
 
 // 59. Using Google, StackOverflow and MDN.
 
-// Given an array of temperatures of one day, calculate the temperature amplitude.
+// Given an array of temperatures of one day, calculate the temp amplitude.
 // Keep in mind that sometimes there might be a sensor error.
 
 // 1. Understand the problem.
@@ -13,7 +13,8 @@
 // 1.2. What values are valid for the temperature amplitude to be calculated ?
 
 // 2.1. Iterate over the temperatures / array /.
-// 2.2. Check if the temperature is actually a number, throw error, if is anything else.
+// 2.2. Check if the temperature is actually a number,
+// throw error, if is anything else.
 // 2.3. If temperature is valid, calculate the temperature amplitude (somehow).
 
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
@@ -21,8 +22,9 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 const calcTempAmplitude = function (temperatures) {
   let maxTemp = Number.MIN_SAFE_INTEGER;
   let minTemp = Number.MAX_SAFE_INTEGER;
-  //   let maxTemp = temperatures[0]; // what if i[0] is an 'error' ? -> NaN, bug in our code.
-  //   let minTemp = temperatures[0]; // what if i[0] is an 'error' ? -> NaN, bug in our code.
+  //  let maxTemp = temperatures[0];
+  //  let minTemp = temperatures[0];
+  //  what if i[0] is an 'error' ? -> NaN, bug in our code.
   for (let i = 0; i < temperatures.length; i++) {
     const currentTemp = temperatures[i];
 
@@ -50,16 +52,21 @@ const tempAmplitudes = calcTempAmplitudes([
   [10, 12],
   [-20, -21],
 ]);
+
 console.log(tempAmplitudes);
+console.log(calcTempAmplitude(tempAmplitudes));
+
 const tempAmplitude = calcTempAmplitude(temperatures);
 
 console.log(tempAmplitude);
 console.log(calcTempAmplitude([10, 12]));
+console.log(calcTempAmplitude([2, 1]));
 console.log(calcTempAmplitude([0, -6]));
 console.log(calcTempAmplitude([-1, -6]));
 console.log(calcTempAmplitude([1, -6]));
 console.log(calcTempAmplitude([-20, -21]));
-// console.log(calcTempAmplitude(['error', -20, -21]));
+// console.log(calcTempAmplitude(['error', -20, -21])); ->
+// that's why I didn't use  'max = arr[0]'.
 
-// Now, the proj manager says "The function should recieve 2 arrays of temperatures.
+// Now, the proj manager says "The function should recieve 2 arrays of temps.
 // The rest of the function should work just the same."
