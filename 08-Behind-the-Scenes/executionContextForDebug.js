ad56f7db886d304cbd9fa732c22ca7b479555df4
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-20 23:25:36 +0200

    closure

--- 08-Behind-the-Scenes/executionContextForDebug.js
+++ 08-Behind-the-Scenes/executionContextForDebug.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // BLOCK SCOPED FUNCTION
 if (true) {
   const sayHi = () => console.log('Hi');
@@ -29,33 +28,34 @@ function second(x, y) {
   return c;
 }
 
-const third = function () {
-  // Closure -> 'ahola' function ? WTF ?
-  console.log('hi third');
-  console.log(firstName);
+const test = function () {
+  const test1 = () => {
+    aloha();
+    // third();
+    return 0;
+  };
+
+  const aloha = (z, y) => {
+    return z + y;
+  };
+
+  const third = () => {
+    console.log('hi third');
+    console.log(firstName);
+  };
+
+  const f = () => {
+    // Closure -> 'ahola' function ? WTF ?
+    console.log('hi third');
+    console.log(firstName);
+  };
+  third();
+  f();
+
+  test1();
 };
 
-third();
-
-/**
- * @param {number} z
- * @param {number} y
- * @return {number} 0;
- */
-const aloha = (z, y) => {
-  return z + y;
-};
-
-/**
- *@return {number} 1
- */
-const asd = () => {
-  console.log('hi fourth');
-  const u = aloha();
-  // because 'aloha' func is called inside of a function,
-  // I have the func in 'third' func's closure. ??
-  return u;
-};
+test();
 
 const first = function () {
   let a = 1;
@@ -69,7 +69,7 @@ const first = function () {
   // here we call second func (that's why it's shown in the closure)
 
   // console.log(c); Error
-  const b = second(7, 9);
+  // const b = second(7, 9);
   // const u = aloha();
   console.log(c);
 
