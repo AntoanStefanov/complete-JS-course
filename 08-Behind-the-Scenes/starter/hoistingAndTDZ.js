10dce13b3d3a0001fdec9ca29fed5d7ab33d5784
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-21 18:34:49 +0200

    hoisting & TDZ

---
+++ 08-Behind-the-Scenes/starter/hoistingAndTDZ.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// console.log(y); // Error. Cannot access 'y' before initialization.
+// const y = 3; // Initialization.
+
+// console.log(x); // -> hoisted with undefined.
+// var x = 1;
+
+console.log('a');
+const z = 1;
+console.log(z);
