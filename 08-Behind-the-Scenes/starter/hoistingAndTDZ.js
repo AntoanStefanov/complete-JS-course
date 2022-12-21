38eafbe17133ac9b49f95528b262a52ef027e242
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-21 20:57:32 +0200

    hoisting

--- 08-Behind-the-Scenes/starter/hoistingAndTDZ.js
+++ 08-Behind-the-Scenes/starter/hoistingAndTDZ.js
@@ -22,16 +22,53 @@
 // console.log(`I am a ${job}`); // Err:Cannot access 'job' before init.
 // const job = 'learner'; // here 'job' is defined. Block Scoped.
 
-const myName = 'Antoan';
+// const myName = 'Antoan';
 
-if (myName === 'Antoan') {
-  // ***************TEMPORAL DEAD ZONE for job variable.***********************
-  console.log(`${myName} is a ${job}`); // Err:Cannot access 'job' before init.
-  // Error is not thrown in debug mode. IF U WAIT A BIT BEFORE CONTINUING...
-  // -> Antoan is a undefined, breakpoint: 29
-  const age = 2022 - 2000;
-  console.log(age);
-  // ***************TEMPORAL DEAD ZONE for job variable.***********************
-  const job = 'learner'; // here 'job' is defined here. Block Scoped.
-  console.log(x); // Err: 'x' is not defined.
+// if (myName === 'Antoan') {
+//   ***************TEMPORAL DEAD ZONE for job variable.***********************
+//   console.log(`${myName} is a ${job}`); // Err:Can't access 'job' before init
+//   // Error is not thrown in debug mode. IF U WAIT A BIT BEFORE CONTINUING...
+//   // -> Antoan is a undefined, breakpoint: 29
+//   const age = 2022 - 2000;
+//   console.log(age);
+//   ***************TEMPORAL DEAD ZONE for job variable.***********************
+//   const job = 'learner'; // here 'job' is defined here. Block Scoped.
+//   console.log(x); // Err: 'x' is not defined.
+// }
+
+// Variables
+/*
+console.log(me);
+console.log(job);
+console.log(birthYear);
+
+var me = 'Antoan';
+let job = 'learner';
+const birthYear = 2000;
+*/
+
+// Functions
+
+/*
+console.log(addDecl(1, 1));
+console.log(addExpr(1, 1));
+console.log(addArrow(1, 1));
+*/
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ * */
+/*
+function addDecl(a, b) {
+  return a + b;
 }
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b; // return a + b, IMPLICITLY.
+*/
