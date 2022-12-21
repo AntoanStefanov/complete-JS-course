7ee7c975e3190ab3a22242e9bdffabe42d858949
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-21 09:18:21 +0200

    .gitignore change, scopes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,4 +1,25 @@
 'use strict';
 
-// 89. An High-Level Overview of JavaScript!
-// 90. The JavaScript Engine and Runtime !!!
+// 93. Scoping in Practice.
+
+/**
+ * Function Declaration(Hoisted).
+ * Defined in global scope. / Top-Level Code /
+ * @param {number} birthYear
+ * @return {number} age
+ */
+function calcAge(birthYear) {
+  // Function scope
+  /*
+  https://dev.to/pranav016/advanced-javascript-series-part-42-scope-chains-and-their-working-lexical-and-variable-environments-19d5
+    The variable environment maps the local scope of a given environment.
+    In other words, the variable environment stores those variables defined
+    within the given working code block {}.
+  */
+  const age = 2027 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+const firstName = 'Antoan';
+calcAge();
