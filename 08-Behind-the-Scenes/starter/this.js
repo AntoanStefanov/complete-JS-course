a7afb883754f04b1621c286255f71cac052f3628
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-22 13:24:42 +0200

    this -> https://eslint.org/docs/latest/rules/no-invalid-this

--- 08-Behind-the-Scenes/starter/this.js
+++ 08-Behind-the-Scenes/starter/this.js
@@ -14,9 +14,42 @@ const antoan = {
   // Do NOT get own 'this'.
   showYear: () => console.log(this.year), // in debug 'this' is defined,
   // but, in debug console 'this' here is 'window' object.
+  // https://eslint.org/docs/latest/rules/no-invalid-this ! REASONS FOR THAT !
 };
 
 antoan.calcAge();
 antoan.showYear();
 
 // ******************************************
+
+// // Event handler with Arrow function -> this === window object.
+// const hOneEl = document.getElementsByTagName('h1')[0];
+// hOneEl.addEventListener('click', (ev) => {
+//   console.log(ev.target.innerHTML);
+//   // For this inside functions, this rule basically checks
+//   // whether or not the function containing this keyword is
+//   // a constructor or a method.
+//   // Note that arrow functions have lexical this,
+//   // and that therefore this rule checks their enclosing contexts.
+//   // No error here. /lexical this here is the window.obj/
+//   console.log(this.innerHTML);
+// });
+
+// // Event handler with function expressions -> this === the HTMLElement.
+// hOneEl.addEventListener('click', function (ev) {
+//   // Maybe better to use ev.target, instead of 'this' in EVENT HANDLERS.
+//   console.log(ev.target.innerHTML);
+//   // ESlint error: Unexpected 'this'
+//   // https://eslint.org/docs/latest/rules/no-invalid-this
+//   // "Under the strict mode, this keywords outside of classes or
+//   // class-like objects might be undefined and raise a TypeError.""
+//   console.log(this.innerHTML); // this: h1Element.
+//   // For this inside functions, this rule basically checks
+//   // whether or not the function containing this keyword is
+//   // a constructor or a method.
+//   // Note that arrow functions have lexical this,
+//   // and that therefore this rule checks their enclosing contexts.
+//   // No error here. /lexical this here is the window.obj/
+// });
+
+// console.log(this);
