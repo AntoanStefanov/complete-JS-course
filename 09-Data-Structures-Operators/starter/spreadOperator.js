235abe2182fff4176a994961a5203f6bf5199071
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-24 16:50:14 +0200

    spreadOperator in action

---
+++ 09-Data-Structures-Operators/starter/spreadOperator.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const arr = [7, 8, 9];
+console.log(arr);
+
+// let's create a new array based on this array,
+// but with some new items at the beginning.
+
+const newArr = [5, 6, ...arr];
+console.log(newArr);
+console.log(...newArr);
+
+const restaurant = {
+  restaurantName: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (startMenuIndex, mainMenuIndex) {
+    return [this.starterMenu[startMenuIndex], this.mainMenu[mainMenuIndex]];
+  },
+
+  //             destructuring object, right away. We got 4 variable names.
+  orderDelivery: function ({
+    mainIndex = 0,
+    starterIndex = 1,
+    time = '20:00',
+    address,
+  }) {
+    console.log(time, address, mainIndex, starterIndex);
+    console.log(`Order recieved! ${this.starterMenu[starterIndex]} & \
+${this.mainMenu[mainIndex]} will be delivered to "${address}" at ${time}`);
+  },
+};
+
+restaurant.mainMenu = [...restaurant.mainMenu, 'Steak'];
+console.log(restaurant.mainMenu);
+
+// Two important use cases of ... spread operator,
+
+// 1. Creating shallow copy of arrays.
+// 2. Merge two arrays together.
+
+// 1.
+const copyOfMainMenu = [...restaurant.mainMenu];
+copyOfMainMenu.push('Lazagna');
+
+console.log(restaurant.mainMenu);
+console.log(copyOfMainMenu);
+
+// 2.
+const allMenuItems = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(allMenuItems);
+
+// Spread Operator works on all so-called iterables.
+// Iterables are things like arrays, strings, maps, sets, but NOT objects.
+
+// Iterable objects are objects that can be iterated over with for..of.
+// Technically, iterables must implement the Symbol.iterator method.
+// Debug to see IT, the Symbol.iterator method.!
+
+const str = 'Antoan';
+const lettersArray = [...str];
+console.log(lettersArray);
