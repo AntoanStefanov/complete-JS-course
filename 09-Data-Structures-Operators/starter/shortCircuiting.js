da25a16cc5d40796ff882e23483d0a145dd79d81
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-24 20:55:08 +0200

    initial

---
+++ 09-Data-Structures-Operators/starter/shortCircuiting.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// AND & OR logical operators.
+// How to use them for short circuit.
+// Until now, we used && and ||  to combine boolean values.
+// We can do more than that with these logical operators.
