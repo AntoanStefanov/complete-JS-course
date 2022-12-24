89c3d562fd2eef38a9e3f6cf566045004f49f7f0
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-24 21:38:53 +0200

    short circuiting with OR operator

--- 09-Data-Structures-Operators/starter/shortCircuiting.js
+++ 09-Data-Structures-Operators/starter/shortCircuiting.js
@@ -27,3 +27,73 @@ console.log(3 || 'Antoan'); // output: 3
 
 const x = false || 0 || ''; // output: '' (empty string) /last value/
 console.log(x);
+
+// That works this way because
+// OR -> if at least one operand is true -> the result is true.
+// If one of the operands is true, we 'short-circuit' the evaluation.
+
+const restaurant = {
+  restaurantName: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (startMenuIndex, mainMenuIndex) {
+    return [this.starterMenu[startMenuIndex], this.mainMenu[mainMenuIndex]];
+  },
+
+  //             destructuring object, right away. We got 4 variable names.
+  orderDelivery: function ({
+    mainIndex = 0,
+    starterIndex = 1,
+    time = '20:00',
+    address,
+  }) {
+    console.log(time, address, mainIndex, starterIndex);
+    console.log(`Order recieved! ${this.starterMenu[starterIndex]} & \
+${this.mainMenu[mainIndex]} will be delivered to "${address}" at ${time}`);
+  },
+
+  orderPasta: function (ingredient, taste, boxForHome) {
+    console.log(
+      `You order pasta with ${ingredient} which taste is ${taste}.\
+Box for Home: ${boxForHome ? 'yes' : 'no'}. Restaurant: ${this.restaurantName}`,
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    let str = `Pizza made with ${mainIngredient}`;
+    for (const ingredient of otherIngredients) {
+      str += ', ' + ingredient;
+    }
+    console.log(str);
+  },
+};
+
+// More practical application of it.
+// Let's say, we do not know if that prop exists.
+
+// const numberOfGuests = restaurant.numberOfGuests
+//   ? restaurant.numberOfGuests
+//   : 0;
+
+// 'short-circuiting' & OR operator, instead of ternary operator (above).
+const numberOfGuests = restaurant.numberOfGuests || 0;
+console.log(numberOfGuests);
+// Easier method to set default values, instead of ternary operator, or even
+// worse if-else statement.
