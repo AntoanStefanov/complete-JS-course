1f174b3af8c495dde1947bb94c2012ca25307091
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-26 18:37:59 +0200

    data & data structures overview

---
+++ 09-Data-Structures-Operators/starter/summaryWhichDataStructureToUse.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Dealing and working with data is the main thing we do as DEVs.
+
+// We have been working with JS built-in data structures,
+// First, we learned about Arrays & Objects.
+// Second, we learned about Sets & Maps.
+
+// Now, we have 4 data structures from which we can choose.
+
+// Let's see the prons & cons of each data structure,
+// Let's see when to choose each of them.
+
+// Where data can actually come from?
+// UI -> user interface/the webpage.
+// 3 sources of data.
+
+// 1. From the program itself: Data written directly in source code/status messg
+// 2. From the UI: Data input from the user or data written in DOM/tasks in todo
+// 3. From external sources: Data fetched e.g. from web API/e.g. recipe objects/
+
+// Web API -> Aplication Programming Interface, we can use web API, to get data,
+// from other web applications.
+// E.g. We could use a web API to get the current weather in any city or data
+// about movies, or anything you want.
+
+// So, no matter where the data comes from or what kind of data it is.
+// We usually always have collections of data, that we then, need to store
+// somewhere.
+
+// So, where do we store collections of data ? Answer: DATA STRUCTURES.
