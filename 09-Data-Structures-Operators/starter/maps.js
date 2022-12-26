bd64cbba4a444200649a5eed9f2b14a2dfb12369
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-26 17:05:33 +0200

    map fundamentals

--- 09-Data-Structures-Operators/starter/maps.js
+++ 09-Data-Structures-Operators/starter/maps.js
@@ -66,19 +66,50 @@ const restaurant = new Map(); // Map constructor // empty map
 // add item to map.
 restaurant.set('name', 'Perfecto');
 restaurant.set('setup', 'Outdoor/Indoor');
-// key as any type, example:
-const returnedValueFromSet = restaurant.set(
-  ['Levski', 'Pavlikeni'],
-  'location',
-);
-console.log(returnedValueFromSet); // the map itself.
 
 // set(...), returns the map iself.
 restaurant
   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
   .set('open', 11)
-  .set('close', 23);
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
 
 console.log(restaurant);
 console.log([...restaurant]); // [[key:value], [key:value], [key:value], ...]
 // map is an iterable, so spread operator works(...)
+
+console.log(restaurant.get('name'));
+console.log(restaurant.get(true));
+
+const time = 9;
+console.log(
+  restaurant.get(
+    time > restaurant.get('open') && time < restaurant.get('close'),
+  ),
+);
+
+console.log(restaurant.has('name'));
+console.log(restaurant.delete('setup'));
+// restaurant.clear();
+console.log(restaurant.size);
+
+// Let's use array(objects) as map keys.
+// key as any type, example:
+restaurant.set(['Levski', 'Pavlikeni'], 'location');
+
+// That does not work., objects have different memory addresses.
+// The two arrays are NOT the same object in the heap.
+console.log(restaurant.get(['Levski', 'Pavlikeni']));
+
+// In order to make it work, we need to use the same object.
+const cities = ['Levski', 'Pavlikeni'];
+restaurant.set(cities, 'location');
+console.log(restaurant.get(cities));
+// 'cities' refers to the same place in memory heap.
+
+// This could be useful with DOM elements.
+// Which are nothing more than just a special type of object.
+
+restaurant.set(document.querySelector('h1'), 'Heading');
+console.log(restaurant);
