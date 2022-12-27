316b0106e6e452fb7faaa29ed464761b415c8b42
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-27 15:31:28 +0200

    slice method

--- 09-Data-Structures-Operators/starter/workingWithStringsOne.js
+++ 09-Data-Structures-Operators/starter/workingWithStringsOne.js
@@ -29,4 +29,22 @@ console.log(airline.slice(4)); // 'Air Portugal' substr /start from 4(inclusive)
 console.log(airline.slice(4, 7)); // 'Air'| from 4(inclusive) to 7(exclusive)
 // the length of the extracted string is always gonna be the end(7)-beginning(4)
 
+// Until now, we hard-coded these values .slice(4, 7).
+// But of course, many times, we don't even know the string, that we recieve.
+
+// Let's try to extract just the first word of the string, without knowing,
+// indexes.
+
+// First word
+console.log(airline.split(' ')[0]); // split()->an array | [0] ->TAP
+console.log(airline.slice(0, airline.indexOf(' '))); // -> TAP
+
+// Last word.
+console.log(airline.split(' ')); // split()->an array | [0] ->TAP
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); // -> Portugal
+
+console.log(airline.slice(-2)); // -> 'Portugal' -> al
+console.log(airline.slice(0, -8)); // -> TAP Air
+console.log(airline.slice(-3, -1)); // -> 'Portugal' -> ga
+
 
