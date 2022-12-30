67dacfeb661f7be859c54bb636b6e0bd82e8eaa7
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-30 19:49:59 +0200

    closure practice

--- 10-Functions/starter/codeChallengeTwo.js
+++ 10-Functions/starter/codeChallengeTwo.js
@@ -3,7 +3,7 @@
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
-  document.body.addEventListener('click', function (e) {
+  document.body.addEventListener('click', function () {
     header.style.color = 'blue';
   });
 })();
