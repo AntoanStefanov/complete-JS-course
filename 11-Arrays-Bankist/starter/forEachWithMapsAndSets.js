d4c817141aa0dea4955662e15511ce5df9de7d35
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-31 14:41:32 +0200

    map.forEach

---
+++ 11-Arrays-Bankist/starter/forEachWithMapsAndSets.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// We learned about the forEach method on arrays,
+// however, forEach is also available on maps and sets.
+
+// Map
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// console.log(currencies);
+
+// currencies.forEach((value) => console.log(value));
+
+// currencies.forEach((value, key) =>
+//   console.log(`Value: ${value} | Key: ${key}`),
+// );
+
+// currencies.forEach((value, key, map) => {
+//   console.log(`Value: ${value} | Key: ${key} | Map: ${map}`);
+// });
