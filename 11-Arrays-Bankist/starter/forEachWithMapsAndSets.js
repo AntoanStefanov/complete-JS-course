119a940f052ac58be4b9b60888618038d5c06241
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-31 14:54:32 +0200

    set foreach

--- 11-Arrays-Bankist/starter/forEachWithMapsAndSets.js
+++ 11-Arrays-Bankist/starter/forEachWithMapsAndSets.js
@@ -21,3 +21,18 @@
 // currencies.forEach((value, key, map) => {
 //   console.log(`Value: ${value} | Key: ${key} | Map: ${map}`);
 // });
+
+// Set
+const currenciesUnique = new Set(['USD', 'EUR', 'GBP', 'EUR', 'GBP', 'EUR']);
+console.log(currenciesUnique);
+
+// Value is EXACTLY the SAME as the key. BECAUSE
+// A set does NOT have keys.
+// A set does NOT have indexes too.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/forEach
+// We can use '_' instead of 'key' as variable,
+// bcs '_' means a throwaway variable(unnecessary variable).
+// (value, _, set) => {...}
+currenciesUnique.forEach((value, key, set) => {
+  console.log(`Value: ${value} | Key: ${key} | Map: ${set}`);
+});
