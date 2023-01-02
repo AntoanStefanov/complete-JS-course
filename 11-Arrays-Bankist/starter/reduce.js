396760d22f09ccae5d2fe07dce395ee9bca77a96
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-02 20:55:49 +0200

    reduce

---
+++ 11-Arrays-Bankist/starter/reduce.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const totalBalance = movements.reduce(
+  (totalBalance, movement) => totalBalance + movement,
+);
+
+console.log(totalBalance);
