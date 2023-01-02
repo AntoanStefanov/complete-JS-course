ed40fdbdda072ef513f89b9af3c6bbb295f4c81d
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-02 21:38:14 +0200

    reduce

--- 11-Arrays-Bankist/starter/reduce.js
+++ 11-Arrays-Bankist/starter/reduce.js
@@ -3,7 +3,15 @@
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const totalBalance = movements.reduce(
-  (totalBalance, movement) => totalBalance + movement,
+  (currentTotalBalance, movement) => currentTotalBalance + movement,
   0,
 );
 console.log(totalBalance);
+
+// Get Maximum value.
+// if (movement > maxMovement) return movement;
+// return maxMovement;
+const maxMovement = movements.reduce((currentMaxMovement, movement) =>
+  movement > currentMaxMovement ? movement : currentMaxMovement,
+);
+console.log(maxMovement);
