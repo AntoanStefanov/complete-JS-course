efefe3db3a0bd7154fb08aa5b3e8ccba4cafb1be
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-03 14:35:05 +0200

    find method

--- 11-Arrays-Bankist/starter/theFindMethod.js
+++ 11-Arrays-Bankist/starter/theFindMethod.js
@@ -7,3 +7,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // returns the first element in the array that satisfies the condition.
 console.log(movements.find((movement) => movement > 1000));
+
+// Find method
+// const account = accounts.find((account) => account.owner === 'Jessica Davis');
+// console.log(account);
