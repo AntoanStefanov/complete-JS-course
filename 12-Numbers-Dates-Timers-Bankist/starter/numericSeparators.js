e13acc6964c971c4c43609180c556ff7a5364cc8
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-07 13:40:29 +0200

    numericSep

---
+++ 12-Numbers-Dates-Timers-Bankist/starter/numericSeparators.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// format numbers in a way that is easier to read.
+
+const number = 287_460_000_000;
+const decimalNum = 287_460_000_000.05;
+console.log(number);
+console.log(decimalNum);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFree = 15_00;
+console.log(transferFree);
