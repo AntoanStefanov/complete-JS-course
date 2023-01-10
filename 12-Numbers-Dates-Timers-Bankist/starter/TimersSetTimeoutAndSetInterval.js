08df8696fd50e073769d8e2117426f5cb6650ffc
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-10 10:23:38 +0200

    passing args to setTimeout, cancelling timeout

--- 12-Numbers-Dates-Timers-Bankist/starter/TimersSetTimeoutAndSetInterval.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/TimersSetTimeoutAndSetInterval.js
@@ -8,10 +8,29 @@
 // We can use setTimeout, to execute some code at some point in the future.
 
 // callback, time in milliseconds (1000 milliseconds = 1 second)
-setTimeout(() => console.log('Here is the Pizza Delivery'), 3000);
+setTimeout(() => console.log('Here is the Pizza Delivery'), 1000);
 console.log('Waiting');
-// scheduling a function call, 3 second later.
+// scheduling a function call, 1 second later.
 // When JS hits the setTimeout, it will keep counting the time in the background
 // and register the callback function, to be called after time has elapsed.
 // and then JS'll immediately move on to the next line.
 // This mechanism is called Asynchronous Javascript.
+
+// What if we need to pass some arguments to the callback function?
+// https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
+// setTimeout(functionRef, delay, param1, param2, /* … ,*/ paramN)
+const food = 'Steak';
+setTimeout((food) => console.log(`Here is the ${food} Delivery`), 2000, food);
+
+// the 'this' problem.
+// https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#the_this_problem
+// wrapper function, bind
+
+// We can cancel a timeout
+const timeoutID = setTimeout(
+  (food) => console.log(`Here is the ${food} Delivery`),
+  3000,
+  'Pizza',
+);
+
+clearTimeout(timeoutID);
