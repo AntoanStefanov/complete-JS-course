dce2b05fc2a7fe717178bf27daead4db2ee44eb9
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-11 20:39:55 +0200

    mdn docs, querySelector remind

--- 13-Advanced-DOM-Bankist/starter/selectingCreatingDeletingElements.js
+++ 13-Advanced-DOM-Bankist/starter/selectingCreatingDeletingElements.js
@@ -1,6 +1,22 @@
 'use strict';
 
 // selecting the document itself of any webpage.
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement
 console.log(document.documentElement);
 // example: applying CSS styles to the entire page, we always need to select
 // documentElement.
+
+// developer.mozilla.org/en-US/docs/Web/API/Document/head
+https: console.log(document.head);
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/body
+console.log(document.body);
+
+// For the above special elements, we don't need to write any selector.
+// But otherwise, as I already know, we can use querySelector.
+
+console.log(document.querySelector('html'));
+console.log(document.querySelector('head'));
+console.log(document.querySelector('body'));
+
+// querySelector -> returns first match
+// querySelectorAll -> returns all matches
