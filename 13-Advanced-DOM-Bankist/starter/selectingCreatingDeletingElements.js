b5c542b5e0ef98cbeaee5facdd150d2a01ad2a70
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-11 20:11:22 +0200

    selecting documentElement

---
+++ 13-Advanced-DOM-Bankist/starter/selectingCreatingDeletingElements.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// selecting the document itself of any webpage.
+console.log(document.documentElement);
+// example: applying CSS styles to the entire page, we always need to select
+// documentElement.
