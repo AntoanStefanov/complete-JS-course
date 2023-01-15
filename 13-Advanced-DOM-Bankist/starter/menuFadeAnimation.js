987f827981a3bd897e955024d153e0f42c3edbe6
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-15 14:09:18 +0200

    important

--- 13-Advanced-DOM-Bankist/starter/menuFadeAnimation.js
+++ 13-Advanced-DOM-Bankist/starter/menuFadeAnimation.js
@@ -1,17 +1,26 @@
 'use strict';
 
-const nav = document.querySelector('.nav');
-console.log(nav);
 /**
- * Nav Hover Handler. Event delegation.
- * @param {Event} event
+ * Menu Fade Animation.
  */
-function navHoverHandler(event) {
-  const target = event.target;
+function menuFadeAnimation() {
+  const nav = document.querySelector('.nav');
+  console.log(nav);
+  /**
+   * Nav Hover Handler. Event delegation.
+   * @param {Event} event
+   */
+  function navHoverHandler(event) {
+    const target = event.target;
 
-  if (target.tagName.toLowerCase() !== 'a') return;
+    if (target.tagName.toLowerCase() !== 'a') return;
+  }
 
-  console.log('hover');
+  nav.addEventListener('mouseover', navHoverHandler);
+  // mouseover is similar to mouseenter, with the BIG diffenence that,
+  // mouseenter EVENT does not bubble. (same event going upwards to the root.)
+  // !!!!!!!!!!!!!!! DOCS !!!!!!!!!!!!!!!!! READ IT.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseenter_event#usage_notes
 }
 
-nav.addEventListener('mouseover', navHoverHandler);
+menuFadeAnimation();
