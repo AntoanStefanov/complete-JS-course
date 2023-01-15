fdd45cd0c6cb4fe88aa454f28454a40d4854a028
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-15 21:08:28 +0200

    sticky nav

--- 13-Advanced-DOM-Bankist/starter/stickyNavigation.js
+++ 13-Advanced-DOM-Bankist/starter/stickyNavigation.js
@@ -1,1 +1,15 @@
 'use strict';
+
+/**
+ * Sticky navigation. Using scroll event for now.
+ */
+function stickyNavigation() {
+  // scroll event is not really efficient, should be avoided.
+  // to many events dispatched for a small amount of scrolling.
+  // For now, we'll use that. There is another way.
+
+  document.addEventListener('scroll', (event) => {
+    console.log(event);
+  });
+}
+stickyNavigation();
