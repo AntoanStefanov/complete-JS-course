ca0305348cb9a9cc6c34c8d6101c7ccc22e98edd
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-15 18:03:34 +0200

    refactoring

--- 13-Advanced-DOM-Bankist/starter/menuFadeAnimation.js
+++ 13-Advanced-DOM-Bankist/starter/menuFadeAnimation.js
@@ -5,22 +5,52 @@
  */
 function menuFadeAnimation() {
   const nav = document.querySelector('.nav');
-  console.log(nav);
+
   /**
-   * Nav Hover Handler. Event delegation.
    * @param {Event} event
+   * @param {String} action
    */
-  function navHoverHandler(event) {
+  function hoverHandler(event, opacity = '100%') {
     const target = event.target;
 
-    if (target.tagName.toLowerCase() !== 'a') return;
+    // There are no child elements that we can accidentally hover over,
+    // so we do NOT use closest method. check tabbedComponent, for more.
+    if (target.classList.contains('nav__link')) {
+      const anchorEl = target;
+
+      const anchorElSiblings = anchorEl
+        .closest('.nav__links')
+        .querySelectorAll('.nav__link');
+
+      const logo = nav.querySelector('#logo');
+
+      anchorElSiblings.forEach((anchor) => {
+        if (anchor !== anchorEl) {
+          anchor.style.opacity = opacity;
+        }
+      });
+
+      logo.style.opacity = opacity;
+    }
+
+    // if (ElTagName !== 'a' || ElTagName !== 'img') return;
+    // We cannot use this above, bcs Always will be => ElTagName === 'nav'
+    // we first hover over the nav to reach the a tag, so this will always return
   }
 
-  nav.addEventListener('mouseover', navHoverHandler);
+  // https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648983#questions
+  // 16:10
+  // nav.addEventListener('mouseover', hoverHandler.bind('60%'));
+  // nav.addEventListener('mouseover', hoverHandler.bind(nav, '60%'));
+
+  nav.addEventListener('mouseover', (event) => hoverHandler(event, '60%'));
+  nav.addEventListener('mouseout', (event) => hoverHandler(event));
+
   // mouseover is similar to mouseenter, with the BIG diffenence that,
   // mouseenter EVENT does not bubble. (same event going upwards to the root.)
   // !!!!!!!!!!!!!!! DOCS !!!!!!!!!!!!!!!!! READ IT.
   // https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseenter_event#usage_notes
+  // opposites: mouseenter -> mouseleave, mouseover -> mouseout
 }
 
 menuFadeAnimation();
