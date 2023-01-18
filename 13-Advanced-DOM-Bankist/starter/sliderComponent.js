6814a5e7aacd3291a9e220f689786fd27cf89d8d
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-18 14:47:02 +0200

    rebuilding slider

--- 13-Advanced-DOM-Bankist/starter/sliderComponent.js
+++ 13-Advanced-DOM-Bankist/starter/sliderComponent.js
@@ -1,5 +1,23 @@
 'use strict';
 
+// If slideNum = 0:
+// 100 * (0 - 0) = 100 * 0 = 0%
+// 100 * (1 - 0) = 100 * 1 = 100%
+// 100 * (2 - 0) = 100 * 2 = 200%
+// 100 * (3 - 0) = 100 * 3 = 300%
+
+// If slideNum = 1:
+// 100 * (0 - 1) = 100 * -1 = -100%
+// 100 * (1 - 1) = 100 * 0 = 0%
+// 100 * (2 - 1) = 100 * 1 = 100%
+// 100 * (3 - 1) = 100 * 2 = 200%
+
+// If slideNum = 2:
+// 100 * (0 - 2) = 100 * -2 = -200%
+// 100 * (1 - 2) = 100 * -1 = -100%
+// 100 * (2 - 2) = 100 * 0 = 0%
+// 100 * (3 - 2) = 100 * 1 = 100%
+
 /**
  * Slider Component.
  * https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649001#questions
@@ -51,6 +69,49 @@ function sliderComponent() {
   };
 
   btnRight.addEventListener('click', rightBtnHandler);
+  btnLeft.addEventListener('click', leftBtnHandler);
+}
+
+// sliderComponent();
+
+/**
+ * Slider Component.
+ * https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649001#questions
+ */
+function sliderComponent() {
+  const slides = document.querySelectorAll('.slide');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const btnLeft = document.querySelector('.slider__btn--left');
+
+  let currentSlideNum = 0;
+
+  const goToCurrentSlide = () => {
+    slides.forEach((slide, index) => {
+      slide.style.transform = `translate(${100 * (index - currentSlideNum)}%)`;
+    });
+  };
+
+  // Initialize slider.
+  goToCurrentSlide();
+
+  const rightBtnHandler = function () {
+    currentSlideNum++;
+
+    if (currentSlideNum === slides.length) currentSlideNum = 0;
+
+    goToCurrentSlide();
+  };
+
+  const leftBtnHandler = function () {
+    currentSlideNum--;
+
+    if (currentSlideNum === -1) currentSlideNum = slides.length - 1;
+
+    goToCurrentSlide();
+  };
+
+  btnRight.addEventListener('click', rightBtnHandler);
+
   btnLeft.addEventListener('click', leftBtnHandler);
 }
 
