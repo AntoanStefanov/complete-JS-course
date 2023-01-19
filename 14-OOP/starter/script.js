cab3aec15cc2a404b35de80c01fd19f45e9f39a7
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-19 21:00:48 +0200

    prototype

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -33,3 +33,26 @@
 
 // https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649033#questions/13444264
 // 9:49.
+
+// 207. OOP in JavaScript
+
+// 1. Prototypes.
+
+// Prototype.
+
+// All objects in JS are linked to a certain prototype object. We say:
+// Each object has a prototype.
+
+// The prototype object contains methods and properties,
+// that all the objects that are linked to that prototype can access and use.
+
+// This behavior is usually called 'prototypal inheritance'.
+
+// All methods and properties of the prototype object/образец/ are accessible to
+// all objects that (have/are linked to) that prototype/образец/.
+
+// Objects inherit methods and properties from that prototype. /образец/
+
+// const test = {};
+// test.__proto__ -> Object
+// test.__proto__.__proto__ -> null
