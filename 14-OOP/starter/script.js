f393eff52a6a0298e323155aec47776bd0469d64
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-19 17:45:38 +0200

    OOP introg

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// 206. What is Object-Oriented Programming?
+
+// A class itself is not an object.
+// chertej za kushta -> realna kushta.
+// Shablon(class) -> Obekt(instance/real object/)
+// blueprint -> instance
+
+// The beauty of this, is that we can use this blueprint
+// to create as many instances/objects/ as we want.
+
+// these instances/objects/ can have different data/properties/,
+// but they share the same functionality/methods/.
