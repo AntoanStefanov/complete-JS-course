b68b571e63838e38d5ff8a88e8d15cbc0cc823cd
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-19 20:19:44 +0200

    4 fundamental principles

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -12,3 +12,24 @@
 
 // these instances/objects/ can have different data/properties/,
 // but they share the same functionality/methods/.
+
+// How do we actually design a class?
+// How do we model real-world data into classes?
+
+// Like an architecture student, asking:
+// "How do we actually plan and design a house?"
+
+// The answer is NOT straightforward.
+// There is NO single correct way of designing classes.
+
+// There are, however, 4 fundamental principles of OOP()
+// There are, however, 4 fundamental principles that
+// can guide us towards a good class implementation.
+
+// Abstraction | Encapsulation | Inheritance | Polymorphism
+
+// These techniques can actually be used outside of OOP,
+// but they are especially relevant in this context.
+
+// https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649033#questions/13444264
+// 9:49.
