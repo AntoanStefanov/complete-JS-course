0f8378f353f204033af343e5abacaf924095c649
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-21 12:42:07 +0200

    prototypes, prototypal inheritance

---
+++ 14-OOP/starter/prototypesAndPrototypalInheritance.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function Vehicle(factory, maxSpeed) {
+  this.factory = factory;
+  this.maxSpeed = maxSpeed;
+}
+
+Vehicle.prototype.info = function () {
+  console.log(`Made in ${this.factory}. Max speed: ${this.maxSpeed}`);
+};
+
+function Automobile(factory, maxSpeed, color) {
+  Vehicle.call(this, factory, maxSpeed); // call parent constructor
+  this.color = color;
+}
+
+Automobile.prototype.startEngine = function () {
+  console.log(`Starting engine on car made in ${this.factory}`);
+};
+
+Object.setPrototypeOf(Automobile.prototype, Vehicle.prototype);
+// this sets a __proto__ to the prototype property (of the Constructor function)
+// Now, Vehicle.prototype is __proto__ for Automobile.prototype. (proto chain).
+// So, when we call automobile.info(),
+// it will find it, in Vehicle.prototype property|Automobile.prototype.__proto__
+
+// Created objects with constructor fn, their __proto__ is the fn prototype property,
+// automobile.__proto__ === Automobile.prototype | true
+// automobile.__proto__.__proto__ === Vehicle.prototype | true
+
+// https://stackoverflow.com/questions/6617780/how-to-call-parent-constructor
+
+// OR (NOT REALLY)
+
+// Automobile.prototype = Object.create(Vehicle.prototype); // keeps th proto clean
+// Automobile.prototype.constructor = Automobile; // repair the inherited cnstrctor
+
+const vehicle = new Vehicle('Pegeout', 120);
+vehicle.info();
+
+const automobile = new Automobile('BMW', 150, 'green');
+automobile.info();
