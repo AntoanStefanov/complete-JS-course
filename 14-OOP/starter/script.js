86c13c8e5de0a85a118cacecf77645f2633798cf
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-21 10:32:02 +0200

    prototype chain

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -351,3 +351,67 @@ console.log(carBMW);
 carBMW.brake();
 console.log(carMercedes);
 carMercedes.accelerate();
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
+// #building_longer_inheritance_chains
+
+function Constructor() {}
+
+const obj = new Constructor();
+// obj ---> Constructor.prototype ---> Object.prototype ---> null
+
+function Base() {}
+function Derived() {}
+// Set the `[[Prototype]]` of `Derived.prototype`
+// to `Base.prototype`
+Object.setPrototypeOf(Derived.prototype, Base.prototype);
+
+const obj1 = new Derived();
+// obj ---> Derived.prototype ---> Base.prototype ---> Object.prototype ---> null
+console.log(obj);
+console.log(obj1);
+
+function Vehicle(factory, maxSpeed) {
+  this.factory = factory;
+  this.maxSpeed = maxSpeed;
+}
+
+Vehicle.prototype.info = function () {
+  console.log(`Made in ${this.factory}. Max speed: ${this.maxSpeed}`);
+};
+
+function Automobile(factory, maxSpeed, color) {
+  Vehicle.call(this, factory, maxSpeed); // call parent constructor
+  this.color = color;
+}
+
+Object.setPrototypeOf(Automobile.prototype, Vehicle.prototype);
+// this sets a __proto__ to the prototype property (of the Constructor function)
+// Now, Vehicle.prototype is __proto__ for Automobile.prototype. (proto chain).
+// So, when we call automobile.info(),
+// it will find it, in Vehicle.prototype property|Automobile.prototype.__proto__
+
+// Created objects with constructor fn, their __proto__ is the fn prototype property,
+// automobile.__proto__ === Automobile.prototype | true
+// automobile.__proto__.__proto__ === Vehicle.prototype | true
+
+// https://stackoverflow.com/questions/6617780/how-to-call-parent-constructor
+
+// OR (NOT REALLY)
+
+// Automobile.prototype = Object.create(Vehicle.prototype); // keeps th proto clean
+// Automobile.prototype.constructor = Automobile; // repair the inherited cnstrctor
+
+Automobile.prototype.startEngine = function () {
+  console.log(`Starting engine on car made in ${this.factory}`);
+};
+
+const vehicle = new Vehicle('Pegeout', 120);
+vehicle.info();
+
+const automobile = new Automobile('BMW', 150, 'green');
+automobile.info();
+
+// ES6 Classes
