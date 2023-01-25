b9c90c0f98c8a10184a69b97b525608074701a29
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-25 20:05:56 +0200

    geolocation API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -28,18 +28,27 @@ function app() {
   // 232. Using the Geolocation API
   // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
   // Geolocation API is called API, bcs it is a browser API,
-  // just like the internalization API, timers, anything that the browser provides
+  // just like the internalization API, timers, anything that the browser provides,
+  // API example (internalization => Intl):
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
   function getGeolocation() {
     const geolocation = navigator.geolocation;
 
+    // clause check (for older browsers who have not implemented geolocation)
+    if (!geolocation) return;
+
     const successCallback = function (geolocationPosition) {
       console.log(geolocationPosition);
+      const {latitude, longitude} = geolocationPosition.coords;
+      console.log(latitude, longitude);
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     };
 
     const errorCallback = function (_) {
       alert('Could not get location.');
     };
 
+    // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
     geolocation.getCurrentPosition(successCallback, errorCallback);
   }
 
