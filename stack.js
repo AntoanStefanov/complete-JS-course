93d3010b26ff53eb9b79c1c03fb662eaa7bf419e
AntoanStefanov <antoine.stefanov@gmail.com>
2023-02-06 20:38:02 +0200

    check if brackets are valid, using stack

---
+++ stack.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line strict
+'use strict';
+
+const brackets = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+};
+
+const isBracketOpen = function (bracket) {
+  if (bracket === '{' || bracket === '[' || bracket === '(') return true;
+  return false;
+};
+
+const openBracketsStack = [];
+
+const str = '[{}]';
+const str1 = '[]{}({})';
+const str2 = '{]';
+
+const areBracketsValid = function (str) {
+  for (const bracket of str) {
+    if (isBracketOpen(bracket)) {
+      openBracketsStack.push(bracket);
+    } else {
+      const lastOpenBracket = openBracketsStack.pop();
+      if (!(brackets[lastOpenBracket] === bracket)) {
+        console.log('Invalid.');
+        return;
+      }
+    }
+  }
+  console.log('Valid.');
+};
+
+areBracketsValid(str);
+areBracketsValid(str1);
+areBracketsValid(str2);
