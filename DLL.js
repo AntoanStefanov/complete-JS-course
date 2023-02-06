7045e59d2be8e8c20f9537cebd91cee4f097f460
AntoanStefanov <antoine.stefanov@gmail.com>
2023-02-06 18:10:05 +0200

    edge cases

--- DLL.js
+++ DLL.js
@@ -30,6 +30,20 @@ class DoublyLinkedList {
     return this.#tail;
   }
 
+  #addFirstElement(data) {
+    this.#tail = this.#head = new Node(data, null, null);
+    this.#size++;
+  }
+
+  #isLastElement() {
+    return this.#size === 1;
+  }
+
+  #deleteLastElement() {
+    this.#head = this.#tail = null;
+    this.#size--;
+  }
+
   print() {
     const arrayNodes = [];
     let currentNode = this.#head;
@@ -46,81 +60,94 @@ class DoublyLinkedList {
 
   addLast(data) {
     if (this.isEmpty()) {
-      this.#tail = this.#head = new Node(data, null, null);
-    } else {
-      // newTail: previous->current tail, next->null.
-      const newTail = new Node(data, this.tail, null); // 2 -> 1
-
-      // current tail.next->newTail
-      this.#tail.next = newTail; // 1 -> 2
-
-      // set newTail as tail.
-      this.#tail = newTail;
+      this.#addFirstElement(data);
+      return;
     }
+    // newTail: previous->current tail, next->null.
+    const newTail = new Node(data, this.tail, null); // 2 -> 1
+
+    // current tail.next->newTail
+    this.#tail.next = newTail; // 1 -> 2
+
+    // set newTail as tail.
+    this.#tail = newTail;
+
     this.#size++;
-    return this.tail;
   }
 
   deleteLast() {
-    if (this.isEmpty()) return;
-
-    if (this.#size === 1) {
-      this.#head = this.#tail = null;
-    } else {
-      const previousNode = this.#tail.previous;
-
-      // make previous node's next set to null.
-      previousNode.next = null;
-
-      // set tail to previous node.
-      this.#tail = previousNode;
-
-      // Object deletion. make it accessible to the garbage collector. aka null.
-      // https://stackoverflow.com/questions/16643156/how-to-delete-an-object-from-memory-using-jquery-javascript
-
-      // https://javascript.info/garbage-collection
-      // Garbage Collector will get the unaccessible node.
-      // "Outgoing references do not matter.
-      // Only incoming ones can make an object reachable."
-
-      // both 56 and 59 lines removes the only TWO references to the node.
-      // there are no other ways of getting that node, so with this said,
-      // garbage collector will get it.
+    if (this.isEmpty()) {
+      console.log('DLL is empty!');
+      return;
     }
+
+    if (this.#isLastElement()) {
+      this.#deleteLastElement();
+      return;
+    }
+
+    const previousNode = this.#tail.previous;
+
+    // make previous node's next set to null.
+    previousNode.next = null;
+
+    // set tail to previous node.
+    this.#tail = previousNode;
+
+    // Object deletion. make it accessible to the garbage collector. aka null.
+    // https://stackoverflow.com/questions/16643156/how-to-delete-an-object-from-memory-using-jquery-javascript
+
+    // https://javascript.info/garbage-collection
+    // Garbage Collector will get the unaccessible node.
+    // "Outgoing references do not matter.
+    // Only incoming ones can make an object reachable."
+
+    // both 56 and 59 lines removes the only TWO references to the node.
+    // there are no other ways of getting that node, so with this said,
+    // garbage collector will get it.
 
     this.#size--;
   }
 
   addFirst(data) {
     if (this.isEmpty()) {
-      this.#tail = this.#head = new Node(data, null, null);
-    } else {
-      // newHead: prev:null, next:head
-      const newHead = new Node(data, null, this.#head);
-
-      // head.prev = newHead.
-      this.#head.previous = newHead;
-
-      // set newHead as head.
-      this.#head = newHead;
+      this.#addFirstElement(data);
+      return;
     }
+
+    // newHead: prev:null, next:head
+    const newHead = new Node(data, null, this.#head);
+
+    // head.prev = newHead.
+    this.#head.previous = newHead;
+
+    // set newHead as head.
+    this.#head = newHead;
+
     this.#size++;
   }
 
   deleteFirst() {
-    if (this.isEmpty()) return;
-
-    if (this.#size === 1) {
-      this.#head = this.#tail = null;
-    } else {
-      this.#head.next.previous = null;
-      this.#head = this.#head.next;
+    if (this.isEmpty()) {
+      console.log('DLL is empty!');
+      return;
     }
+
+    if (this.#isLastElement()) {
+      this.#deleteLastElement();
+      return;
+    }
+
+    this.#head.next.previous = null;
+    this.#head = this.#head.next;
     this.#size--;
   }
 
   insert(index, data) {
-    if (index < 0 || index > this.#size) return;
+    if (index < 0 || index > this.#size) {
+      console.log('Invalid index');
+      return;
+    }
 
     if (index === 0) {
       this.addFirst(data);
@@ -163,6 +190,7 @@ DLL.addLast(3);
 DLL.deleteLast();
 DLL.addFirst(0);
 DLL.deleteFirst();
+DLL.print();
 
 const DLL1 = new DoublyLinkedList();
 DLL1.addLast(1);
@@ -174,3 +202,26 @@ DLL1.insert(1, 2);
 DLL1.insert(2, 3);
 DLL1.insert(1, 7); // 1, 7, 2, 3
 DLL1.print();
+
+const DLL2 = new DoublyLinkedList();
+DLL2.insert(1, 1);
+DLL2.deleteFirst();
+DLL2.deleteLast();
+DLL2.insert(0, 1);
+DLL2.print();
+DLL2.deleteLast();
+DLL2.deleteLast();
+DLL2.insert(-1, 1);
+DLL2.addFirst(1);
+DLL2.deleteFirst();
+DLL2.deleteFirst();
+DLL2.print();
+DLL2.addLast(1);
+DLL2.addLast(2);
+DLL2.print();
+DLL2.deleteFirst();
+DLL2.deleteLast();
+DLL2.addFirst(1);
+DLL2.addFirst(0);
+DLL2.insert(1, 7);
+DLL2.print();
